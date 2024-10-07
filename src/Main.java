4fb49e829497d1757ccee18ba039b41435f304e9
NTHrd <hismatovegor@gmail.com>
2024-10-07 22:56:45 +0500

    Добавлена проверка на дурака

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -35,7 +34,7 @@ public class Main {
                             System.out.println("Enter five numbers:");
                             int res = main.lastNumSum(sc.nextInt(), sc.nextInt());
                             for (int i = 0; i < 3; i++) {
-                                System.out.println("Remaining:"+res);
+                                System.out.println("Remaining:" + res);
                                 res = main.lastNumSum(res, sc.nextInt());
                             }
                             System.out.println("Result:" + res);
@@ -104,7 +103,7 @@ public class Main {
                             System.out.println("Enter number which to be added to array:");
                             int x = sc.nextInt();
                             System.out.println("Enter position:");
-                            System.out.println("Result:" + Arrays.toString(main.add(arr1, x, sc.nextInt())));
+                            main.write_arr(main.add(arr1, x, sc.nextInt()));
                             break;
                         case 3:
                             System.out.println("Enter an array of numbers:");
@@ -114,11 +113,11 @@ public class Main {
                             System.out.println("Enter first array:");
                             int[] arr3 = main.read_arr();
                             System.out.println("Enter second array:");
-                            System.out.println("Result:" + Arrays.toString(main.concat(arr3, main.read_arr())));
+                            main.write_arr(main.concat(arr3, main.read_arr()));
                             break;
                         case 5:
                             System.out.println("Enter an array of numbers:");
-                            System.out.println("Result:" + Arrays.toString(main.deleteNegative(main.read_arr())));
+                            main.write_arr(main.deleteNegative(main.read_arr()));
                             break;
                     }
                     break;
@@ -142,12 +141,21 @@ public class Main {
         return (x > 0);
     }
     public boolean isUpperCase (char x) {
+        if (!Character.isLetter(x)) {
+            throw new IllegalArgumentException("Input must be a letter.");
+        }
         return (x >= 'A' && x <= 'Z');
     }
     public boolean isDivisor (int x, int y) {
+        if (x == 0) {
+            throw new IllegalArgumentException("The divisor cannot be zero.");
+        }
         return (y % x == 0);
     }
     public int lastNumSum(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Both numbers must be non-negative.");
+        }
         return ((a % 10) + (b % 10)) % 10;
     }
     //Task 2
@@ -170,6 +178,9 @@ public class Main {
         return ((x + y == z) || (x + z == y) || (y+ z == x));
     }
     public String age (int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Age cannot be negative.");
+        }
         if (x % 10 == 1 && x != 11){
             return (x + " год");
         }
@@ -179,6 +190,9 @@ public class Main {
         return (x + " лет");
     }
     public void printDays (String x) {
+        if (x == null || x.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: day of the week cannot be null or empty.");
+        }
         switch (x) {
             case "понедельник":
                 System.out.println("вторник");
@@ -203,6 +217,9 @@ public class Main {
     }
     //Task 3
     public String reverseListNums (int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Invalid input: number must be non-negative.");
+        }
         String result = "";
         for (int i = x; i >= 0; i--) {
             result += String.valueOf(i) + " ";
@@ -210,13 +227,22 @@ public class Main {
         return result;
     }
     public double pow (int x, int y) {
-        int result = 1;
+        if (y < 0) {
+            throw new IllegalArgumentException("Invalid input: exponent must be non-negative.");
+        }
+        if (y == 0) {
+            return 1;
+        }
+        double result = 1;
         for (int i = 0; i < y; i++) {
             result *= x;
         }
         return result;
     }
     public boolean equalNum (int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Invalid input: number must be non-negative.");
+        }
         int match_num = x % 10;
         while (x != 0){
             if (match_num != x % 10){ return false; }
@@ -225,6 +251,9 @@ public class Main {
         return true;
     }
     public void leftTriangle (int x) {
+        if (x < 1) {
+            throw new IllegalArgumentException("Invalid input: height of the triangle must be a positive integer.");
+        }
         System.out.println("Result:");
         for (int i = 1; i <= x; i++) {
             for (int j = 0; j < i; j++) {
@@ -236,13 +265,19 @@ public class Main {
     public void guessGame() {
         System.out.println("Enter a number from 0 to 9:");
         Scanner sc = new Scanner(System.in);
-        Random rand  = new Random();
+        Random rand = new Random();
         int rand_num = rand.nextInt(9);
-        int num_attempt = 1;
-        while (sc.nextInt() != rand_num) {
+        int num_attempt = 0;
+        int guess;
+        do {
+            guess = sc.nextInt();
+            if (guess < 0 || guess > 9) {
+                System.out.println("Invalid input: guess must be between 0 and 9.");
+                continue;
+            }
             System.out.println("You guessed wrong, enter a number from 0 to 9:");
             num_attempt++;
-        }
+        } while (guess != rand_num);
         System.out.println("You guessed the right number!");
         System.out.println("You guessed in " + num_attempt + " attempts.");
     }
@@ -251,6 +286,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
         String input = sc.nextLine();
+        if (input == null || input.isEmpty()) {
+            return null;
+        }
         String[] stringNumbers = input.replace(",", " ").split(" ");
         for (String str : stringNumbers) {
             int number = Integer.parseInt(str);
@@ -262,7 +300,17 @@ public class Main {
         }
         return arr;
     }
+    public void write_arr (int [] arr) {
+        System.out.println("Result:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public int findLast (int[] arr, int x) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty.");
+        }
         for (int i = arr.length; i > 0; i--) {
             if (arr[i-1] == x) {
                 return i;
@@ -271,6 +319,9 @@ public class Main {
         return -1;
     }
     public int[] add (int[] arr, int x, int pos) {
+        if (pos < 0 || pos > arr.length) {
+            throw new IndexOutOfBoundsException("Position is out of bounds.");
+        }
         int[] res = new int[arr.length + 1];
         for (int i = 0; i < pos; i++) {
             res[i] = arr[i];
@@ -282,14 +333,20 @@ public class Main {
         return res;
     }
     public void reverse (int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Array cannot be null.");
+        }
         for (int i = 0; i < arr.length / 2; i++) {
             int temp = arr[i];
             arr[i] = arr[arr.length - i - 1];
             arr[arr.length - i - 1] = temp;
         }
-        System.out.println("Result:" + Arrays.toString(arr));
+        write_arr(arr);
     }
     public int[] concat (int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) {
+            throw new IllegalArgumentException("Input arrays cannot be null.");
+        }
         int[] res = new int[arr1.length + arr2.length];
         for (int i = 0; i < arr1.length; i++) {
             res[i] = arr1[i];
@@ -300,6 +357,9 @@ public class Main {
         return res;
     }
     public int[] deleteNegative (int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Input array cannot be null.");
+        }
         int cnt = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
